867b51f88cf0b3471b7a8f74a2a2fe1f9f85176c
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-27 14:20:01 -0600

    adding the todo list to the application

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,16 +1,22 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the DataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DataProvider {
-
-  constructor(public http: HttpClient) {
+  lists = [
+    {
+      itemName: 'Milk',
+      checked: false
+    },
+    {
+      itemName: 'Cheese',
+      checked: true
+    },
+    {
+      itemName: 'Bread',
+      checked: false
+    },
+  ];
+  constructor() {
     console.log('Hello DataProvider Provider');
   }
 
